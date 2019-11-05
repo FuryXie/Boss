119044aa9828d4cad395dbca397e260f98d5c8d9
xie <957965803@qq.com>
2019-11-05 22:59:50 +0800

    列表页

---
+++ src/pages/list/List.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <list-main></list-main>
+    </div>
+</template>
+
+<script>
+import ListMain from './components/Main'
+export default {
+    name:'List',
+    components:{
+        ListMain
+    }
+}
+</script>
+
+<style>
+    
+</style>
